c14e60997f3849d0f060b524a068b2eddb80dd02
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-11-13 07:05:31 +0530

    Initial commit
    Generated by create-expo-app 1.0.0.

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your ap!</Text>
-      {/* <StatusBar style='auto' /> */}
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
